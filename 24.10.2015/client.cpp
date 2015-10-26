1f4a3a26dd7ca12366eaf61d021e10b9ba8b8d4b
codeserfer <admin@codeserfer.com>
2015-10-26 23:07:47 +0300

    Made it beautiful

--- 24.10.2015/client.cpp
+++ 24.10.2015/client.cpp
@@ -1,7 +1,6 @@
-// 24.10.2015.cpp: определяет точку входа для консольного приложения.
-//
 // Клиент
-// Сервер должен преобразовать переданный текст в верхний регистр
+// Сервер принимает точку координат центра окружности в виде x, y и радиус, также принимается точка поиска.
+// Возвращает признак вхождения точки поиска внутрь заданной окружности
 // TCP сокет
 
 #include "stdafx.h"
@@ -9,6 +8,7 @@
 
 #include "WinSock2.h"
 #include <iostream>
+#include <string>
 #pragma comment(lib, "Ws2_32.lib")
 
 #define  PORT 4229
@@ -16,82 +16,110 @@
 
 using namespace std;
 
-void CloseConnection(SOCKET connectSocket)
+
+class TCP
 {
-	closesocket(connectSocket);
-	WSACleanup();
-}
+private:
+	SOCKET s;
+	WSAData wsaData;
+
+public:
+	void Startup()
+	{
+		if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 0)
+		{
+			throw string("Initialization failed");
+		}
+	}
+
+	void InitSocket()
+	{
+		s = socket(
+			AF_INET,
+			SOCK_STREAM, //_DGRAM, _RAW
+			0);
+
+		if (!s)
+		{
+			throw string("Error in socket connection");
+		}
+	}
+	
+	void Connect(int port, string host)
+	{
+		DWORD addr = inet_addr(host.c_str());
+		HOSTENT *hinfo = gethostbyaddr((char*)&addr, 4, AF_INET);
+
+		// заполнение структуры для соединения
+		sockaddr_in tcpaddr;
+		tcpaddr.sin_family = AF_INET;
+		tcpaddr.sin_port = htons(port);
+
+		memcpy((char FAR *)&(tcpaddr.sin_addr.s_addr), hinfo->h_addr_list[0], hinfo->h_length);
+
+		if (connect(s, (SOCKADDR*)&tcpaddr, sizeof(tcpaddr)) == -1)
+		{
+			string error = "Connection error: " + to_string(WSAGetLastError());
+
+			throw error;
+		}
+	}
+
+	void SendData(string str)
+	{
+		send(s, str.c_str(), str.length(), 0);
+	}
+
+	string ReceiveData()
+	{
+		char str[1024];
+
+		int countRecieved = recv(s, str, sizeof(str), 0);
+
+		if (countRecieved == SOCKET_ERROR)
+		{
+			string error = "Receiving data error: " + to_string(WSAGetLastError());
+
+			throw error;
+		}
+		str[countRecieved] = '\0';
+
+		return string(str);
+	}
+
+	void CloseConnection()
+	{
+		closesocket(s);
+		WSACleanup();
+	}
+};
+
+
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	WSAData wsaData;
-	int iResult;
-	iResult = WSAStartup(MAKEWORD(1, 1), &wsaData);
+	TCP* tcp = new TCP();
 
-	if (iResult != 0)
+	try 
 	{
-		cout << "Initialization failed" << endl;
-		
-		return 0;
+		tcp->Startup();
+		tcp->InitSocket();
+		tcp->Connect(PORT, HOST);
+
+		cout << "Input string to send: ";
+
+		string s;
+		cin >> s;
+
+		tcp->SendData(s);
+		cout << "Reseive: " << endl;
+		cout << tcp->ReceiveData() << endl;
+		tcp->CloseConnection();
 	}
-
-
-	SOCKET s = socket(
-		AF_INET,
-		SOCK_STREAM, //_DGRAM, _RAW
-		0);
-
-	if (!s)
+	catch (string error)
 	{
-		cout << "Error in socket connection" << endl;
-		cout << WSAGetLastError() << "\n";
-
-		CloseConnection(s);
-
-		return 0;
+		cout << error;
+		tcp->CloseConnection();
 	}
-
-	DWORD addr = inet_addr(HOST);
-	HOSTENT *hinfo = gethostbyaddr((char*)&addr, 4, AF_INET);
-
-	// заполнение структуры для соединения
-	sockaddr_in tcpaddr;
-	tcpaddr.sin_family = AF_INET;
-	tcpaddr.sin_port = htons(PORT);
-
-	memcpy((char FAR *)&(tcpaddr.sin_addr.s_addr), hinfo->h_addr_list[0], hinfo->h_length);
-
-	iResult = connect(s, (SOCKADDR*)&tcpaddr, sizeof(tcpaddr));
-
-	if (iResult == SOCKET_ERROR)
-	{
-		cout << WSAGetLastError() << endl;
-		
-		CloseConnection(s);
-
-		return 0;
-	}
-
-	char str[1024];
-
-	cout << "Input sending text: ";
-	cin >> str;
-
-	send(s, str, strlen(str), 0);
-
-	int countRecieved = recv(s, str, sizeof(str), 0);
-
-	if (countRecieved == SOCKET_ERROR)
-	{
-		cout << WSAGetLastError() << endl;
-
-		CloseConnection(s);
-		
-		return 0;
-	}
-	str[countRecieved] = '\0';
-
-	cout << str;
-
-	CloseConnection(s);
 }
--- 24.10.2015/server.cpp
+++ 24.10.2015/server.cpp
@@ -7,103 +7,131 @@
 
 #include <WinSock2.h>
 #include <iostream>
+#include <algorithm>
 
 #pragma comment(lib, "Ws2_32.lib")
+#include <string>
 
 using namespace std;
 
-
-void CloseConnection(SOCKET s)
+class Server
 {
-	closesocket(s);
-	WSACleanup();
-
-	cout << "Good bue" << endl;
-}
-
-void Server()
-{
+private:
+	SOCKET s;
 	WSAData wsaData;
-	int iResult;
-	iResult = WSAStartup(MAKEWORD(1, 1), &wsaData);
-
-	if (iResult != 00)
-	{
-		cout << "Не удалось инициализировать библиотеку WinSock" << endl;
-		return;
-	}
-
-	//handle или дескриптор - элемент для управления сокетом
-	SOCKET s = socket(
-		AF_INET, // The Internet Protocol version 4 (IPv4) address family.
-		SOCK_STREAM, //A socket type that provides sequenced, reliable, two-way, connection-based byte streams with an OOB data transmission mechanism. This socket type uses the Transmission Control Protocol (TCP) for the Internet address family (AF_INET or AF_INET6).
-		0); //
-
-	// заполнение структур для соединения
 	sockaddr_in tcpaddr;
-	tcpaddr.sin_family = AF_INET;
-	tcpaddr.sin_port = htons(PORT);
-	tcpaddr.sin_addr.s_addr = htons(INADDR_ANY); //слушаем все ip адреса через порт
 
-	//для сервера
-	iResult = bind(s, (SOCKADDR*)&tcpaddr, sizeof(tcpaddr));
+public:
 
-	if (iResult != 0)
+	void Startup()
 	{
-		cout << WSAGetLastError() << endl;
-
-		CloseConnection(s);
-
-		return;
+		if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 00)
+		{
+			throw string("Can't init WinSock lib");
+		}
 	}
 
-	listen(s, 5); //сервер, слушаем сокет, 5 - длина очереди
-
-	char str[1024];
-
-	cout << "Server has been started successfully!\n";
-	cout << "Listening port " << PORT << endl;
-
-	while (true)
+	void CloseConnection()
 	{
+		closesocket(s);
+		WSACleanup();
 
-
-		int buf = sizeof(tcpaddr);
-		SOCKET client = accept(s, (SOCKADDR*)&tcpaddr, &buf);
-
-		int a = recv(client, str, buf, 0);
-
-		if (a == -1)
-		{
-			cout << WSAGetLastError() << endl;
-
-			CloseConnection(s);
-
-			return;
-		}
-		str[a] = '\0';
-
-		cout << "Reseived: ";
-		cout << str << endl;
-
-		if (!strcmp(str, "exit"))
-		{
-			CloseConnection(s);
-		}
-
-
-		strcat_s (str, "_add");
-		send(client, str, strlen(str), 0);
+		cout << "Good bue" << endl;
 	}
-	
-}
+
+	void InitSocket()
+	{
+		//handle или дескриптор - элемент для управления сокетом
+		s = socket(
+			AF_INET, // The Internet Protocol version 4 (IPv4) address family.
+			SOCK_STREAM, //A socket type that provides sequenced, reliable, two-way, connection-based byte streams with an OOB data transmission mechanism. This socket type uses the Transmission Control Protocol (TCP) for the Internet address family (AF_INET or AF_INET6).
+			0); //
+		if (!s)
+		{
+			throw string("Error in socket connection");
+		}
+	}
+
+	void Bind(int port)
+	{
+		// заполнение структур для соединения
+		tcpaddr.sin_family = AF_INET;
+		tcpaddr.sin_port = htons(port);
+		tcpaddr.sin_addr.s_addr = htons(INADDR_ANY); //слушаем все ip адреса через порт
+
+		cout << "Listening port " << port << endl;
+
+		if (bind(s, (SOCKADDR*)&tcpaddr, sizeof(tcpaddr)) != 0)
+		{
+			throw string("Can't bind");
+		}
+	}
+
+	void Listen(int queueString)
+	{
+		listen(s, queueString);
+
+		char str[1024];
+
+		cout << "Server has been started successfully!\n";
+
+		while (true)
+		{
+			int buf = sizeof(tcpaddr);
+			SOCKET client = accept(s, (SOCKADDR*)&tcpaddr, &buf);
+
+			int a = recv(client, str, buf, 0);
+
+			if (a == -1)
+			{
+				string error = "Receiving error: " + to_string(WSAGetLastError());
+
+				throw error;
+			}
+			str[a] = '\0';
+
+			cout << "Reseived: ";
+			cout << str << endl;
+
+			if (!strcmp(str, "exit"))
+			{
+				CloseConnection();
+			}
+
+
+			string strString = string(str);
+
+			transform(strString.begin(), strString.end(), strString.begin(), toupper);
+
+			const char* newString = strString.c_str();
+
+			send(client, newString, strlen(newString), 0);
+		}
+	}
+};
+
+
 
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
 
-	Server();
+	Server* tcp = new Server();
+
+	try
+	{
+		tcp->Startup();
+		tcp->InitSocket();
+		tcp->Bind(PORT);
+		tcp->Listen(10);
+	}
+	catch (string s)
+	{
+		cout << s;
+
+		tcp->CloseConnection();
+	}
 
 	return 0;
 }
