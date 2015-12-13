53615bd13fbceacad46314ed7e6f3129d9440b18
codeserfer <admin@codeserfer.com>
2015-12-13 20:22:20 +0300

    pipe + signals

--- c/2.c
+++ c/2.c
@@ -1,36 +1,30 @@
-#include <unistd.h>
-#include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <math.h>
 
-double CalculateExp(double value, int maxNumberOfOperations)
+int counting_pi = 1;
+int counting_exp = 1;
+int fd[2]; // pipe
+
+void end_counting_pi(int sig)
 {
-
-    printf ("argument is %lf", value);
-    double exp = 1;
-    int i;
-    double factorial = 1;
-    double power;
-
-    for(i = 0; i < maxNumberOfOperations; i++)
-    {
-        power = i + 1;
-        factorial *= power;
-        /*if (i > 300) {
-            printf("%lf", power);
-            printf("\n");
-        }*/
-        exp += pow(value, power)/factorial;
-
-    }
-
-    return exp;
+    printf ("CHILD:  End counting!\n");
+    counting_pi = 0;
 }
 
-double CalculatePI (int maxNumberOfOperations)
+void start_counting_pi(int sig)
 {
+    printf ("CHILD:  Start counting pi!\n");
+    signal(SIGUSR2, end_counting_pi);
+
+
+
+    // counting
     double pi = 1.0;
     int numerator = 2;
     int denominator = 1;
@@ -39,7 +33,7 @@ double CalculatePI (int maxNumberOfOperations)
     int denominator_counter = 1;
 
     unsigned int i;
-    for (i = 0; i<maxNumberOfOperations; i++)
+    while (counting_pi)
     {
         if (numerator_counter == 2)
         {
@@ -58,126 +52,173 @@ double CalculatePI (int maxNumberOfOperations)
         pi = pi* ((double)numerator/(double)denominator);
     }
 
-    return 2*pi;
+    pi = 2*pi;
+
+    printf ("pi is %f\n", pi);
+
+    char text [40];
+    sprintf(text, "@%d pi %f", getpid(), pi);
+
+    printf ("sending text: '%s'\n", text);
+
+    close(fd[0]); // Close unused read end
+    write(fd[1], text, strlen(text));
+
+    close (fd[1]);
+
+    exit (0);
+
 }
 
 
-int main ()
+//
+void end_counting_exp(int sig)
 {
-    int x = 1;
+    printf ("CHILD:  End counting exp!\n");
+    counting_exp = 0;
+}
 
-    int proc1;
+void start_counting_exp(int sig)
+{
+    printf ("CHILD:  Start counting exp!\n");
+    signal(SIGUSR2, end_counting_exp);
 
-    int fd[2]; // pipe
+    // TODO: fix this
+    int x = 2;
+    double value = -(pow(x, 2.0))/2;
 
+    // counting
+    printf ("argument is %lf", value);
+    double exp = 1;
+    int i = 0;
+    double factorial = 1;
+    double power;
+
+    while (counting_exp && i < 300)
+    {
+        power = i + 1;
+        factorial *= power;
+        exp += pow(value, power)/factorial;
+        i++;
+
+    }
+
+
+    printf ("exp is %f\n", exp);
+
+    char text [40];
+    sprintf(text, "%d exp %f", getpid(), exp);
+
+    printf ("sending text: '%s'\n", text);
+
+    close(fd[0]); // Close unused read end
+    write(fd[1], text, strlen(text));
+
+    exit (0);
+
+}
+
+
+void DoNoting ()
+{
+    printf ("pass\n");
+}
+
+
+main()
+{
     if (pipe(fd) < 0)
 	{
 		printf("Cannot create pipe!\n");
 		return 1;
 	}
 
-
-    switch (fork())
+    int pid;
+    if ((pid=fork()) == 0)
     {
-        case 0:
-            printf ("first fork()\n");
-            double exp = CalculateExp(-(pow(x, 2.0))/2, 180);
-            printf ("exp = %f\n", exp);
+        printf ("Child process1 has been started!\n");
+        signal(SIGUSR1, start_counting_pi);
+        while (1);
 
-            char text [40];
-            sprintf(text, "%d exp %f", getpid(), exp);
-
-            close(fd[0]); // Close unused read end
-            write(fd[1], text, strlen(text));
-
-
-            printf ("first fork has written \"%s\" to chanel\n", text);
-
-
-            exit (0);
-        case -1:
-            printf ("Error!!!!!! Close program!");
-            return -1;
-    }
-
-    int proc2;
-    switch (fork())
-    {
-        case 0:
-            printf ("second fork()\n");
-            //double pi = CalculatePI (1000000000);
-            double pi = CalculatePI (100000000);
-            printf ("pi = %f\n", pi);
-
-            char text [40];
-            sprintf(text, "@%d pi %f", getpid(), pi);
-
-            close(fd[0]); // Close unused read end
-            write(fd[1], text, strlen(text));
-
-            close (fd[1]);
-
-            printf ("second fork has written \"%s\" to chanel\n", text);
-
-            exit (0);
-        case -1:
-            printf ("Error!!!!!! Close program!");
-            return -1;
-    }
-
-    wait(&proc1);
-    wait(&proc2);
-
-    printf ("main () go!\n");
-
-
-    int pID1 = 0;
-    int pID2 = 0;
-    char fname1 [4];
-    char fname2 [4];
-    double result1 = 0;
-    double result2 = 0;
-
-
-    close(fd[1]); // Close unused write end
-
-
-    char buf [40];
-    read(fd[0], &buf, sizeof (buf));
-
-    printf ("buf is '%s'\n", buf);
-
-
-    int fscanf_result = sscanf(buf, "%d %s %lf@%d %s %lf", &pID1, fname1, &result1, &pID2, fname2, &result2);
-
-    if (fscanf_result < 6)
-    {
-        printf ("Something went wrong...\n");
-        return 0;
-    }
-
-    printf ("first:  %d  %s  %lf \n", pID1, fname1, result1);
-    printf ("second: %d  %s  %lf \n", pID2, fname2, result2);
-
-    double exp = 0, pi = 0;
-
-    if (!strcmp (fname1, "exp"))
-    {
-        exp = result1;
-        pi = result2;
     }
     else
     {
-        exp = result2;
-        pi = result1;
+
+        int pid2;
+        if ((pid2=fork()) == 0)
+        {
+            printf ("Child process2 has been started!\n");
+            signal(SIGUSR1, start_counting_exp);
+            while (1);
+        }
+        else
+        {
+            printf ("Main pipe!\n");
+            sleep (1);
+            signal(SIGUSR1, DoNoting);
+            signal(SIGUSR2, DoNoting);
+            printf("\nPARENT: sending SIGQUIT\n\n");
+
+            int grp = getpgrp ();
+            kill(-1*grp, SIGUSR1);
+            printf ("grp is %d\n", grp);
+            printf ("child process is %d\n", pid);
+
+            sleep(1);
+            kill(-1*grp, SIGUSR2);
+            sleep (3);
+
+            ////
+            printf ("main () go!\n");
+
+
+            int pID1 = 0;
+            int pID2 = 0;
+            char fname1 [4];
+            char fname2 [4];
+            double result1 = 0;
+            double result2 = 0;
+
+
+            close(fd[1]); // Close unused write end
+
+
+            char buf [40];
+            read(fd[0], &buf, sizeof (buf));
+
+            printf ("buf is '%s'\n", buf);
+
+
+            int fscanf_result = sscanf(buf, "%d %s %lf@%d %s %lf", &pID1, fname1, &result1, &pID2, fname2, &result2);
+
+            if (fscanf_result < 6)
+            {
+                printf ("Something went wrong...\n");
+                return 0;
+            }
+
+            printf ("first:  %d  %s  %lf \n", pID1, fname1, result1);
+            printf ("second: %d  %s  %lf \n", pID2, fname2, result2);
+
+            double exp = 0, pi = 0;
+
+            if (!strcmp (fname1, "exp"))
+            {
+                exp = result1;
+                pi = result2;
+            }
+            else
+            {
+                exp = result2;
+                pi = result1;
+            }
+
+            double final_result = exp/sqrt(2*pi);
+            printf ("result: %lf\n", final_result);
+
+        }
     }
 
-    double final_result = exp/sqrt(2*pi);
-    printf ("result: %lf\n", final_result);
-
-
-    printf ("Close program\n");
-
-    return 0;
+    printf ("end program!\n");
 }
 
