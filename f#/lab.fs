63e1b9fafe1d610c5b52e1b3825cd2e14f64b261
codeserfer <admin@codeserfer.com>
2015-12-17 21:50:49 +0300

    Final relise of archiver

--- f#/lab.fs
+++ f#/lab.fs
@@ -1,10 +1,14 @@
 open System.IO
 
+// Функции чтения из файла
+
 let ReadFile filename =
     File.ReadAllText (filename)
 
 let WriteFile (filename: string, str: string) =
     File.WriteAllText (filename, str)
+
+// Построение дерева кодов и преобразование дерева в таблицу
 
 type Tree =
     | Leaf of int * char
@@ -37,16 +41,6 @@ let rec PrintTree = function
       PrintTree ("0"::code, l)
       PrintTree ("1"::code, r)
 
-let rec GetCodes = function
-  | code, Leaf (f, c), table -> List.append table [String.concat "" (List.rev code)]
-  | code, Node (_, l, r), table ->
-      List.append (List.append table (GetCodes ("0"::code, l, table))) (GetCodes ("1"::code, r, table))
-
-let rec GetLetters = function
-  | code, Leaf (f, c), table -> List.append table [c]
-  | code, Node (_, l, r), table ->
-      List.append (List.append table (GetLetters ("0"::code, l, table))) (GetLetters ("1"::code, r, table))
-
 let rec CreateTable = function
   | code, Leaf (f, c) ->  [(c, String.concat "" (List.rev code))]
   | code, Node (_, l, r) -> List.concat [CreateTable ("0"::code, l); CreateTable ("1"::code, r)]
@@ -56,9 +50,15 @@ let LetterFrequency str =
   |> Seq.countBy (fun letter -> letter)
   |> Map.ofSeq
 
+// Преобразования битовых последовательностей.
+
 let rec ToOctets (str : string) =
-    if str.Length % 8 = 0 then str 
+    if str.Length % 7 = 0 then str 
     else ToOctets (str + "0")
+
+let rec RestoreOctets (str : string) =
+    if str.Length % 7 = 0 then str 
+    else RestoreOctets ("0" + str)
 
 let rec BinaryToDecimal (binary : string) decimal =
     if binary.Length = 0 then decimal
@@ -68,114 +68,98 @@ let rec BinaryToDecimal (binary : string) decimal =
             | '1' -> BinaryToDecimal (binary.Substring 1) (decimal * 2 + 1)
             | _   -> 0   
 
+let rec DecimalToBinary decimal =
+    match decimal with
+    | 0 | 1 -> string decimal
+    | _ ->
+        let bit = string (decimal % 2)
+        (DecimalToBinary (decimal / 2)) + bit
+
 let rec EncodedToBytes (str : string) (numberOfOctets : int) octetCounter =
     if octetCounter = numberOfOctets then []
-    else List.append [byte (BinaryToDecimal str.[0..7] 0)] (EncodedToBytes str.[8..str.Length - 1] numberOfOctets (octetCounter + 1))
+    else List.append [byte (BinaryToDecimal str.[0..6] 0)] (EncodedToBytes str.[7..str.Length - 1] numberOfOctets (octetCounter + 1))
 
-[<EntryPoint>]
-let main argv = 
-//    if argv.Length <> 3
-//    then
-//        printf "%s" "Error!"
-//        -1
-//    else
-//        let inputFile = argv.GetValue(0)
-//        let outputFile = argv.GetValue(1)
-//        let mode = argv.GetValue(2)
-//        0
-    
-    //let input = ReadFile "input.txt"
+// Главные функции.
 
-    let input = "aaabbgvvg"
-
+let EncodeMain inputFile outputFile =
+    let input = ReadFile (inputFile)
     let tree = LetterFrequency input |> Map.toList |> BuildTree
-    //printfn "Symbol\tWeight\tHuffman code";
-    //PrintTree ([], tree)
+
+    printfn "Symbol\tWeight\tHuffman code";
+    PrintTree ([], tree)
 
     let table = CreateTable ([], tree)
-
-    let length = table.Length
-    
     let stringTable = List.fold (fun result (letter : char, code : string) ->  result + letter.ToString() + code + "\n") "" table
     
     let Encode letterToEncode =
         (List.find (fun (letter, code) -> letter = letterToEncode) table) |> snd
 
     let result = List.ofSeq input |> List.map Encode |> List.fold (+) ""
-
-
     let resultToOctets = ToOctets result
+    let bytes = EncodedToBytes resultToOctets (resultToOctets.Length /  7) 0
+    let encoded = table.Length.ToString() + "\n" + stringTable + result.Length.ToString() + "\n" + (System.Text.Encoding.ASCII.GetString (List.toArray bytes)) 
 
-    let bytes = EncodedToBytes resultToOctets (resultToOctets.Length /  8) 0
+    WriteFile (outputFile, encoded)
 
-    let output = table.Length.ToString() + "\n" +
-        stringTable +
-        result.Length.ToString() + "\n" +
-        (System.Text.Encoding.ASCII.GetString (List.toArray bytes)) 
+let DecodeMain (inputFile : string) outputFile =
+    use sr = new StreamReader(inputFile)
 
-    printf "%s" output
-
-    WriteFile ("output.txt", output)
-
-    //// Reading file and encoding
-
-
-    printf "\n------------------------------\n\n";
-
-    use sr = new StreamReader ("output.txt")
-
-    // size of table
-    let tableSize = System.Int32.Parse <| sr.ReadLine ()
+    let firstString = sr.ReadLine ()
+    let tableSize = System.Int32.Parse <| firstString
 
     let ReadTable = seq {  
         for i in 1 .. tableSize do 
             yield sr.ReadLine () 
     }
 
-    // table
-    let table = Seq.toArray <| Seq.map (fun (s : string) -> (s.[0],s.[1..])) ReadTable
+    let decodingTable = Seq.toArray <| Seq.map (fun (s : string) -> (s.[0],s.[1..])) ReadTable
 
-    // lenght of coded text
     let textLenght = System.Int32.Parse <| sr.ReadLine()
-
 
     let ReadText = seq {
         while not sr.EndOfStream do
             yield sr.ReadLine ()
     }
 
-    // coded text
-    let codedText = (String.concat "" (Seq.toArray ReadText)).ToCharArray()
+    let encodedText = (String.concat "" (Seq.toArray ReadText)).ToCharArray()
+    let readBytes = System.Text.Encoding.ASCII.GetBytes encodedText
+    let binaryCodedText = Array.map (fun c -> RestoreOctets (DecimalToBinary c)) <| Array.map (fun c -> (int c)) encodedText
+    let binaryCodedTextSingleString = String.concat "" binaryCodedText
+    let textToDecode = binaryCodedTextSingleString.[0..textLenght-1]
 
-    let rec IntToBinary i =
-        match i with
-        | 0 | 1 -> string i
-        | _ ->
-            let bit = string (i % 2)
-            (IntToBinary (i / 2)) + bit
+    let GetCode codeToDecode =
+        (Array.find (fun (letter, code) -> code = codeToDecode) decodingTable) |> fst 
 
+    let mutable decoded = "" 
 
-    let binaryCodedText = Array.map (fun c -> IntToBinary c) <| Array.map (fun c -> (int c)) codedText
+    let rec DecodeString ttable (stringToDecode: string) =
+        for line in ttable do
+            if stringToDecode.Length <> 0 && stringToDecode.StartsWith(snd line) then
+                decoded <- decoded + string (fst line)
+                (DecodeString ttable stringToDecode.[(snd line).Length..stringToDecode.Length - 1])
 
-    // concatinating and cuted
-    // Это бинарный код почему-то не совпадает с исходным!
-    let binaryString = (binaryCodedText |> Array.fold (+) "").[0..(textLenght-1)]
+    DecodeString decodingTable textToDecode
 
+    WriteFile (outputFile, decoded)
 
+// Main.
 
+[<EntryPoint>]
+let main argv = 
+    if argv.Length <> 3
+    then
+        printf "%s" "Error!"
+        -1
+    else
+        let inputFile = string (argv.GetValue(0))
+        let outputFile = string (argv.GetValue(1))
+        let mode = string (argv.GetValue(2))
 
-    // Тут мои попытки написать перевод кодов по таблице в символы.
-    let rec codesToChars (resultString: string, table, binaryString: string) = 
-        for i in table do
-            let code : string = snd i
-            if (binaryString.Length >= code.Length && binaryString.Substring(0, code.Length).Equals (code)) then
-                codesToChars (resultString + "blo", table, binaryString.Substring(code.Length))
-        resultString
-
-
-    codesToChars ("", table, binaryString)
-
-    // for debuf point   
-    printf "ff"
-
-    0
+        if mode = "-e" then
+            EncodeMain inputFile outputFile
+        else if mode = "-d" then
+            DecodeMain inputFile outputFile
+        else
+            printf "%s" "Error!"
+        
+        0
